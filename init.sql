4ca7c67e9a464a45e91b5432411b24e3e7734dac
Lazaro <danielalorenzoramos@gmail.com>
2026-05-18 20:51:02 -0500

    fix: explicit creation of evolution_db to avoid prisma conflicts

--- init.sql
+++ init.sql
@@ -1,7 +1,8 @@
 -- init.sql
 -- Este script se ejecuta automáticamente si el volumen de PostgreSQL está vacío.
--- PostgreSQL ya crea 'asistencia_db' por la variable POSTGRES_DB en docker-compose,
--- por lo que aquí nos aseguramos de crear la segunda base de datos para el backend.
 
 SELECT 'CREATE DATABASE backend_db'
 WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'backend_db')\gexec
+
+SELECT 'CREATE DATABASE evolution_db'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'evolution_db')\gexec
